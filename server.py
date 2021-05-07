2827fbbc763960f2f5407d7d77f1ada9ce063402
sahil <souravjangar91@gmail.com>
2021-05-07 23:10:16 +0530

    corrected templates

--- server.py
+++ server.py
@@ -1,38 +1,11 @@
 from flask import Flask, render_template, url_for
-app = Flask(__name__)
+portfo = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    print(url_for('static', filename='bolt.ico'))
-    return 'hihihi'
-
-@app.route('/hello')
-def newpage():
+@portfo.route('/')
+def homePages():
     return render_template("index.html")
 
-@app.route('/projects/')
-def projects():
-    return 'The project page'
+@portfo.route('/<page>')
+def otherPages(page):
+    return render_template(f"{page}")
 
-@app.route('/about')
-def about():
-    return 'The about page'
-
-@app.route('/jjj')
-def index():
-    
-    return 'index'
-
-@app.route('/login')
-def login():
-    return "login"
-
-@app.route('/user/<username>')
-def profile(username):
-    return '{}\'s profile'.format(escape(username))
-
-# with app.test_request_context():
-#     print(url_for('index'))
-#     print(url_for('login'))
-#     print(url_for('login', next='/'))
-#     print(url_for('profile', username='John Doe'))
